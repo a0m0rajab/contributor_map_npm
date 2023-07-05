5cc83298c20e9908c7e5ec097491327cc65c0083
Abdurrahman Rajab <rajab-2@windowslive.com>
2023-07-05 20:05:53 +0300

    feat: add octokit

--- index.js
+++ index.js
@@ -1,27 +1,68 @@
 // write a function that takes: accessToken, name, page and returns a list of contributors
 // https://developer.github.com/v3/repos/#list-contributors
 
+const { Octokit, App } = require("octokit");
+const octokit = new Octokit({ auth: `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` });
 
-async function  getContributors(name, page) {
-    const url = `https://api.github.com/repos/${name}/contributors?per_page=100&page=${page}`;
-    const contributors = [];
-    let x = await fetch(url);   
-    // print data from the fetch on screen
-    
-    console.log(x)
-    do {
-        contributors.concat(await fetch(url)) ;
-        const filtered = contributors.filter(c => c.login === "octocat");
-        if (filtered.length > 0) {
-            return filtered;
+
+async function getContributors(name) {
+    const {
+        data: { login },
+      } = await octokit.rest.users.getAuthenticated();
+      console.log("Hello, %s", login);
+      const iterator = octokit.paginate.iterator(octokit.rest.repos.listContributors, {
+        owner: "tensorflow",
+        repo: "tensorflow",
+        per_page: 100,
+      });
+      
+      // iterate through each response
+      for await (const { data: users } of iterator) {
+        for (const user of users) {
+          console.log("User #%d: %s", user.id, user.login);
         }
-    } while (false);
+      }
+    let page = 1;
+    let contributors = [];
+    let list = [];
+   
+    do {
+        let x = await fetch(`https://api.github.com/repos/${name}/contributors?per_page=100&page=${page}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                
+            }});   
+        // print data from the fetch on screen
+        list = await x.json();
+        contributors = contributors.concat(list);
+        debugger;
+        page++;
+    } while (list.length%100 == 0 && list.length != 0);
+    console.log(contributors.length);
+    return contributors;
 }
 
-getContributors("tensorflow/tensorflow", 1)
+async function getContributors2(name) {
+    let contributors = await getContributors("tensorflow/tensorflow")
+    let locations = getLocations(contributors);
+debugger;
+}
 
+function getLocations(contributors) {
+    let locations = [];
+    for (let i = 0; i < contributors.length; i++) {
+        
+    };}
+
+getContributors2("tensorflow/tensorflow");
 
 // https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repository-contributors
 // https://github.com/tunaitis/contributor-map/blob/master/internal/github/github.go
 // https://api.github.com/repos/tensorflow/tensorflow/contributors?per_page=100&page=5
 // https://www.freecodecamp.org/news/how-to-create-and-publish-your-first-npm-package/
+
+// octokit: 
+// https://github.com/octokit/octokit.js#octokitrest-endpoint-methods
+// https://github.com/octokit/plugin-rest-endpoint-methods.js/blob/main/docs/repos/listContributors.md
+// https://github.com/octokit/octokit.js#octokitrest-endpoint-methods
